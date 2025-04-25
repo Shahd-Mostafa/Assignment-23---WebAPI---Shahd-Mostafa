23b5edea79c18511027b4baa5a0973560f3fae05
Shahd  Mostafa <shahd.mustafa124@gmail.com>
2025-04-26 02:24:40 +0300

    V6 - session 5

--- Assignment 23 - WebAPI - Shahd Mostafa/Extentions/PresentationServicesExtensions.cs
+++ Assignment 23 - WebAPI - Shahd Mostafa/Extentions/PresentationServicesExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Assignment_23___WebAPI___Shahd_Mostafa.Factories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 namespace Assignment_23___WebAPI___Shahd_Mostafa.Extentions
 {
@@ -12,8 +13,37 @@ namespace Assignment_23___WebAPI___Shahd_Mostafa.Extentions
             {
                 options.InvalidModelStateResponseFactory = ApiresponseFactory.CustomValidationError;
             });
+            services.AddSwaggerConfiguration();
+            return services;
+        }
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In= ParameterLocation.Header,
+                    Description = "Please Enter Your Bearer token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },new List<string>() { }
+                    }
+                });
+            });
             return services;
         }
     }
