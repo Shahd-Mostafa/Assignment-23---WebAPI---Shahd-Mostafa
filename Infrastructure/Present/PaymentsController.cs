ea5393d43c981e06da0e44587fd04ad385e91b73
Shahd  Mostafa <shahd.mustafa124@gmail.com>
2025-05-06 16:00:11 +0300

    V4 - session 7

---
+++ Infrastructure/Present/PaymentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Present
+{
+    public class PaymentsController(IServiceManager _serviceManager) : ApiController
+    {
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<BasketDto>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            var paymentIntent = await _serviceManager.paymentServices.CreateOrUpdatePaymentIntentAsync(basketId);
+            return Ok(paymentIntent);
+        }
+    }
+}
